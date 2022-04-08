4fe7d3f479af403b0af86d3ab19df88b5e7ad3fc
Ryabtseva-Ekaterina <ekaterina-ryabtseva.evp@yandex.ru>
2022-04-08 12:19:17 +0300

    feat: добавить валидацию форм

--- pages/index.js
+++ pages/index.js
@@ -129,3 +129,69 @@ profileAddButton.addEventListener('click', function () {
 });
 formElementEdit.addEventListener('submit', formSubmitHandler);
 formElementAdd.addEventListener('submit', addCard);
+
+
+
+
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add ('popup__container-form-input_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__container-form-input-text-error');
+ }; 
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove ('popup__container-form-input_error');
+  errorElement.classList.remove('popup__container-form-input-text-error');
+  errorElement.textContent=" ";
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some ((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput (inputList)) {
+    buttonElement.classList.add('popup__container-form-button_inactive');
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.classList.remove('popup__container-form-button_inactive');
+    buttonElement.disabled = false;
+  }
+};
+
+const isValid = (formElement, inputElement) => {
+  if(!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__container-form-input'));
+  const buttonElement = formElement.querySelector('.popup__container-form-button');
+  toggleButtonState(inputList, buttonElement); 
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener ('input', () => {
+      isValid(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__container-form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener ('submit', (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
